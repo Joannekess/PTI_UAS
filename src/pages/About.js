d4f9fb5b61f57b4681f89e28607823d165f76ad1
Joannekess <joannekessie@gmail.com>
2021-12-16 02:43:13 +0700

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,14 @@ import '../css/About.css';
 class About extends React.Component{
     render(){
       return (
-        <h1>Ini About</h1>
+        <div className='about'>
+        <h2>About GercepNews</h2>
+          <div className='justify-content-center d-flex pt-5 row'>
+            <div id="bulbasaur" class="pokemon col-4"></div>
+            <div id="charmander" class="pokemon col-4"></div>
+            <div id="squirtle" class="pokemon col-4"></div>
+          </div>
+        </div>
       );
     }
   }
